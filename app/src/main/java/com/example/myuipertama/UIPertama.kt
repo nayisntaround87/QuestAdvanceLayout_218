0032c0ee83439f96a2387ce9601191b5511c5a47
nayisntaround87 <naylasalwahayati@gmail.com>
2025-10-23 08:51:52 +0700

    Initial commit

---
+++ app/src/main/java/com/example/myuipertama/UIPertama.kt
@@ -0,0 +1,68 @@
+package com.example.myuipertama
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import java.lang.reflect.Modifier
+
+@Composable
+fun ActivitasPertama(modifier: Modifier) {
+    Column(modifier = Modifier.padding(top =100.dp)
+        .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = stringResource(id = R.string.prodi),
+            fontsize = 35.sp,
+            fontWeight = FontWeight.Bold)
+        Text(text =  stringResource(id = R.string.univ),
+            fontSize = 22.sp)
+        Spacer(modifier = Modifier.height(height = 25.dp))
+        Card (modifier = Modifier
+            .fillMaxWidth(fraction = 1f)
+            .padding(all = 12.dp),
+            colors = CardDefaults.cardColors(
+                containerColor = Color.DarkGray
+            )) {
+            Row() {
+                val gambar = painterResource(id = R.drawable.LogoUmy)
+                Image(
+                    painter = gambar,
+                    contentDescription = null,
+                    modifier = Modifier.size(100.dp).padding(5.dp)
+                )
+                Spacer(modifier = Modifier.width(30.dp))
+                Column() {
+                    Text(
+                        stringResource(R.string.nama),
+                        fontSize = 30.sp,
+                        fontFamily = FontFamily.Cursive,
+                        color = Color.White,
+                        modifier = Modifier.padding(top = 15.dp)
+                    )
+
+                }
+            }
+        }
+
+    }
+}
