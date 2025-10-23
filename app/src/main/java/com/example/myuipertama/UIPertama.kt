c7609c3e305cc3de433b435e8952cd3934a7fc01
nayisntaround87 <naylasalwahayati@gmail.com>
2025-10-23 09:54:36 +0700

    mengubah warna biru pd bagian alamat

--- app/src/main/java/com/example/myuipertama/UIPertama.kt
+++ app/src/main/java/com/example/myuipertama/UIPertama.kt
@@ -69,7 +69,7 @@ fun ActivitasPertama(modifier: Modifier) {
                     Text(
                         stringResource(R.string.alamat),
                         fontSize = 20.sp,
-                        color = Color.Yellow,
+                        color = Color.Blue,
                         modifier = Modifier.padding(top=10.dp)
                     )
                 }
