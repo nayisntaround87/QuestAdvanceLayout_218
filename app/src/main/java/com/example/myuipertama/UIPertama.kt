1ca7abfcb28f8b49b8f8fd173e1c4c7ae7105044
nayisntaround87 <naylasalwahayati@gmail.com>
2025-10-23 09:56:11 +0700

    mengubah warna gray di copy

--- app/src/main/java/com/example/myuipertama/UIPertama.kt
+++ app/src/main/java/com/example/myuipertama/UIPertama.kt
@@ -81,6 +81,7 @@ fun ActivitasPertama(modifier: Modifier) {
         ) {
             Text(
                 stringResource(R.string.copy),
+                color = Color.Gray,
                 modifier = Modifier
                     .align(Alignment.BottomCenter)
                     .padding(bottom = 50.dp)
