c4290672bb4d23975f13452cc7487708e4b5666e
nayisntaround87 <naylasalwahayati@gmail.com>
2025-10-23 10:05:05 +0700

    mengubah letter spacing

--- app/src/main/java/com/example/myuipertama/UIPertama.kt
+++ app/src/main/java/com/example/myuipertama/UIPertama.kt
@@ -64,7 +64,7 @@ fun ActivitasPertama(modifier: Modifier) {
                         fontFamily = FontFamily.Cursive,
                         color = Color.White,
                         modifier = Modifier.padding(top = 15.dp),
-                        letterSpacing = 1.sp
+                        letterSpacing = 2.sp
                     )
                     Text(
                         stringResource(R.string.alamat),
